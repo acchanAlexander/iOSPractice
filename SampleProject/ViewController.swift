602d927476d57800d062b81dc00be08c6916a39a
acchanAlexander <AtariYoshihiro@next-group.jp>
2018-07-31 18:28:27 +0900

    print hello world

--- SampleProject/ViewController.swift
+++ SampleProject/ViewController.swift
@@ -13,13 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World!")
+        let label = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        label.text = "Hello World!"
+        self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
