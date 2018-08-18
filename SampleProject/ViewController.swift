d1c71d8aee91ac192c5285eac8cfe3b9a4cbe744
acchanAlexander <AtariYoshihiro@next-group.jp>
2018-08-18 15:47:18 +0900

    Revert "ラベルとボタンの配置"
    This reverts commit 66780ffcf7e65e8c397436093f5949d28a66e797.

--- SampleProject/ViewController.swift
+++ SampleProject/ViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
-    @IBAction func sayHello() {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
